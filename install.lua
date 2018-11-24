63eb8fb22180560b8b8fb60e8dc766293dc936f5
Erd <erd@conceptual.ca>
2018-11-24 10:51:37 -0500

    converting to oc-oop

--- install.lua
+++ install.lua
@@ -2,26 +2,29 @@ local shell = require("shell");
 local fs = require("filesystem");
 local os = require("os");
 print("Downloading...")
-shell.execute('wget -fq "https://raw.githubusercontent.com/rater193/OpenComputers-1.7.10-Base-Monitor/master/lib/json.lua" "/lib/json.lua"');
+shell.execute('wget -fq "https://raw.githubusercontent.com/myrddraall/oc-lib/master/lib/json.lua" "/usr/lib/json.lua"');
 
-fs.makeDirectory('/lib/soul');
-shell.execute('wget -fq "https://raw.githubusercontent.com/majesty2450/OpenComputers-SOUL/0.3.1/Mixin.lua" "/lib/soul/Mixin.lua"');
-shell.execute('wget -fq "https://raw.githubusercontent.com/majesty2450/OpenComputers-SOUL/0.3.1/Class.lua" "/lib/soul/Class.lua"');
-shell.execute('wget -fq "https://raw.githubusercontent.com/mpeterv/argparse/0.6.0/src/argparse.lua" "/lib/argparse.lua"');
-shell.execute('wget -fq "https://raw.githubusercontent.com/myrddraall/oc-git/stable/lib/git.lua" "/lib/git.lua"');
+fs.makeDirectory('/usr/lib/oop');
+shell.execute('wget -fq "https://raw.githubusercontent.com/myrddraall/oc-oop/master/lib/middleclass.lua" "/usr/lib/oop/middleclass.lua"');
+shell.execute('wget -fq "https://raw.githubusercontent.com/myrddraall/oc-oop/master/lib/Class.lua" "/usr/lib/oop/Class.lua"');
+shell.execute('wget -fq "https://raw.githubusercontent.com/mpeterv/argparse/0.6.0/src/argparse.lua" "/usr/lib/argparse.lua"');
+shell.execute('wget -fq "https://raw.githubusercontent.com/myrddraall/oc-git/master/lib/git.lua" "usr/lib/git.lua"');
 
 
-local GitRepo = require("git");
-local oopGit = GitRepo:new("majesty2450/OpenComputers-SOUL");
-oopGit:checkout("/lib/soul");
+--[[
 
-local gitGit = GitRepo:new("myrddraall/oc-git");
-gitGit:checkoutTag("stable", "/tmp/git-install/git");
-
-print("Installing...");
-
-shell.execute("cp /tmp/git-install/git/cli/git.lua /bin/");
-
-print("Rebooting...")
-os.sleep(2);
-shell.execute('reboot');
+    local GitRepo = require("git");
+    local oopGit = GitRepo:new("majesty2450/OpenComputers-SOUL");
+    oopGit:checkout("/lib/soul");
+    
+    local gitGit = GitRepo:new("myrddraall/oc-git");
+    gitGit:checkoutTag("stable", "/tmp/git-install/git");
+    
+    print("Installing...");
+    
+    shell.execute("cp /tmp/git-install/git/cli/git.lua /bin/");
+    
+    print("Rebooting...")
+    os.sleep(2);
+    shell.execute('reboot');
+    ]]
