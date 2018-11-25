78b37a69281dd87f29f03b7be16c4b5f5c8b3d16
Erd <erd@conceptual.ca>
2018-11-25 13:17:55 -0500

    installdir support

--- install.lua
+++ install.lua
@@ -1,3 +1,9 @@
 local shell = require("shell");
-shell.execute('rm /usr/lib/git.lua');
-shell.execute('cp -r ./lib/* /usr/lib/');
+local args = {...};
+local installDir = "";
+if args[1] then
+    installDir = args[1];
+end
+
+shell.execute('rm ' .. installDir .. '/usr/lib/git.lua');
+shell.execute('cp -r ./lib/* ' .. installDir .. '/usr/lib/');
